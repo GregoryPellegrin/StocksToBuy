bfc39c8b04c0c877e78f1e165b881c450e6257fe
Gregory <Gregory@hyperion>
2014-06-03 02:05:41 -0400

    Update

--- StocksToBuy/src/main/java/Servlet/WebSite.java
+++ StocksToBuy/src/main/java/Servlet/WebSite.java
@@ -18,13 +18,11 @@ import javax.servlet.http.HttpServletResponse;
 public final class WebSite extends HttpServlet
 {
 	private String forward;
-	private String design;
 	
 	@Override
 	public void init ()
 	{
 		this.forward = "stockValue";
-		this.design = "light";
 	}
 	
 	private void setForward (final HttpServletRequest request)
@@ -35,22 +33,12 @@ public final class WebSite extends HttpServlet
 			this.forward = request.getParameter("section");
 	}
 	
-	private void setDesign (final HttpServletRequest request)
-	{
-		this.design = "light";
-		
-		if ((request.getParameter("design") != null) && (! request.getParameter("design").isEmpty()))
-			this.design = request.getParameter("design");
-	}
-	
 	@Override
 	protected void service (HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
 		this.setForward(request);
-		this.setDesign(request);
 		
 		request.setAttribute("forward", this.forward);
-		request.setAttribute("design", this.design);
 		
 		RequestDispatcher requestDispatcher = request.getRequestDispatcher("index.jsp");
 		requestDispatcher.forward(request, response);
